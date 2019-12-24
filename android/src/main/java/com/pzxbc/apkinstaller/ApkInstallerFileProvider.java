89ef6143a2218751d80b2f49160528b91b377f2d
gzpengzhangxiang <gzpengzhangxiang@corp.netease.com>
2019-12-24 21:03:00 +0800

    init code

---
+++ android/src/main/java/com/pzxbc/apkinstaller/ApkInstallerFileProvider.java
@@ -0,0 +1,6 @@
+// ApkInstallFileProvider.java
+package com.pzxbc.apkinstaller;
+import android.support.v4.content.FileProvider;
+
+public class ApkInstallerFileProvider extends FileProvider {
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { ApkInstaller } = NativeModules;
+
+export default ApkInstaller;
